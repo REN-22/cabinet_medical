b13566794cf093879fdeac24db4e2c703b1835b5
REN-22 <renzo.bastide@gmail.com>
2023-12-01 12:11:50 +0100

    supprimer rdv opti

--- application/rendezvous/supprimerRdv.php
+++ application/rendezvous/supprimerRdv.php
@@ -1,11 +1,7 @@
 <?php
 require("../components/configDB.php");
 
-$requete = 'DELETE FROM rendez_vous WHERE id_usager = ' . $_GET['id_usager'] . ' 
-AND id_medecin = ' . $_GET['id_medecin'] . '
-AND date_RV = "' . $_GET['date_RV'] . '"
-AND heure_RV = "' . $_GET['heure_RV'] . '"
-AND duree = "' . $_GET['duree'] . '"';
+$requete = 'DELETE FROM rendez_vous WHERE id_rdv = ' . $_GET['id_rdv'];
 
 if (!$resquery = mysqli_query($link, $requete)) {
     die("Error:" . mysqli_errno($link) . ":" . mysqli_error($link));
