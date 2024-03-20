7c96d627e4b0a7ef54cf30911062c51827c80bc7
REN-22 <renzo.bastide@gmail.com>
2024-03-20 10:54:12 +0100

    autentification sur toute l'appli

--- application/index.php
+++ application/index.php
@@ -1,20 +1,3 @@
-<?php
-session_start();
-
-// Votre nom d'utilisateur et mot de passe
-$username = 'admin';
-$hashed_password = password_hash('admin', PASSWORD_DEFAULT);
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if ($_POST['username'] == $username && password_verify($_POST['password'], $hashed_password)) {
-        $_SESSION['loggedin'] = true;
-        header('Location: acceuil.php'); // Redirige vers la page d'accueil
-        exit;
-    } else {
-        $error = "Nom d'utilisateur ou mot de passe incorrect";
-    }
-}
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -23,13 +6,46 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link href="styles.css" rel="stylesheet">
 </head>
 <body>
-<form method="post" action="./index.php">
-    Nom d'utilisateur: <input type="text" name="username"><br>
-    Mot de passe: <input type="password" name="password"><br>
-    <input type="submit" value="Se connecter">
+<form id="loginForm">
+    Nom d'utilisateur: <input type="text" id="username"><br>
+    Mot de passe: <input type="password" id="password"><br>
+    <button type="submit">Se connecter</button>
 </form>
-<?php if (isset($error)): ?>
-<p style="color: red;"><?php echo $error; ?></p>
-<?php endif; ?>
+<p id="errorMessage" style="color: red; display: none;"></p>
+
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+<script>
+document.getElementById('loginForm').addEventListener('submit', async function(event) {
+    event.preventDefault(); 
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    try {
+        const response = await authenticate(username, password);
+        const token = response.data;
+        localStorage.setItem('token', token);
+        window.location.href = './acceuil.php';
+    } catch (error) {
+        console.error(error);
+        document.getElementById('errorMessage').innerText = 'Informations d\'authentification invalides';
+        document.getElementById('errorMessage').style.display = 'block';
+    }
+});
+
+async function authenticate(login, password) {
+    try {
+        const response = await axios.post('http://localhost/cabinet_medical-main/Auth/Auth.php', {
+            login: login,
+            mdp: password
+        });
+        return response;
+    } catch (error) {
+        console.error('Erreur lors de l\'authentification:', error);
+        throw error;
+    }
+}
+</script>
+
 </body>
 </html>
