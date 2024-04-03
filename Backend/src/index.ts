a4a3853406005a20154de2078ae25de61ef0d1fa
REN-22 <renzo.bastide@gmail.com>
2024-04-03 10:00:14 +0200

    usager réparé

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -2,6 +2,7 @@ import express = require('express')
 import { SetUpBD } from './Settings/SetUpBD'
 import { SetUpPortAndCore, app } from './Settings/Settings'
 const medecinRoutes = require('./Routes/Medecins/MedecinRoutes')
+const patientRoutes = require('./Routes/Patients/PatientRoutes').default;
 //import { SetUpBD } from "./Settings/SetUpBD";
 //équipe C7
 
@@ -9,6 +10,7 @@ async function AllSetUp(): Promise<void> {
   /* ----- Set Up Routes ----- */
   app.use(express.json())
   app.use('/medecins', medecinRoutes)
+  app.use('/usagers', patientRoutes)
 
   /* ----- Set Up BD and PORT/CORE ----- */
   await SetUpBD()
